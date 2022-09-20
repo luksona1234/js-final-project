c4b2026c7d23e7252609b2b121338a2838585841
Luka <lukakhachvani0311@gmail.com>
2022-09-21 00:30:02 +0400

    vhgv

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ img.addEventListener('click', () => {
     }
 });
 p20.addEventListener('click', () => {
-    window.scrollTo(0, 3050);
+    window.scrollTo(0, 1470);
 })
 path.addEventListener('click', () => {
     window.scrollTo(0, 3050);
@@ -108,13 +108,15 @@ let boxMonica = document.querySelector('.box-Monica');
 let boxLeon = document.querySelector('.box-Leon');
 let theImageInsideOfTheOrangeBox = document.querySelector('.theImageInsideOfTheOrangeBox')
 let mover1 = document.querySelector('.mover-1')
+let mover2 = document.querySelector('.mover-2');
+let mover3 = document.querySelector('.mover-3');
 let pppp = document.querySelector('.pppp');
 let pppp1 = document.querySelector('.pppp1');
 let box64 = document.querySelector('.div64');
 let div60 = document.querySelector('.div60')
 
 boxSebastian.addEventListener('click', () => {
-    div60.style.display = 'none'
+    div60.style.width = '56%'
     boxSebastian.style.borderRadius = '0%'
     theImageInsideOfTheOrangeBox.style.backgroundImage = 'url(./img6.png)'
     theImageInsideOfTheOrangeBox.style.backgroundposition = 'center'
@@ -131,6 +133,68 @@ boxSebastian.addEventListener('click', () => {
     pppp1.style.color = 'white'
     box64.style.backgroundColor = 'white'
 })
+let pppp2 = document.querySelector('.pppp2');
+let pppp3 = document.querySelector('.pppp3');
+let GrahamLinks = document.querySelector('.GrahamLinks')
+boxGraham.addEventListener('click', () => {
+
+    if(theImageInsideOfTheOrangeBox.style.backgroundImage = 'url(./img6.png)'){
+        theImageInsideOfTheOrangeBox.style.backgroundImage = 'url(./img7.png)'
+        div60.style.width = '47%'
+        mover1.style.width = '55%'
+        mover1.style.height = '100%'
+        mover1.style.marginLeft = '110px'
+        mover1.style.marginTop = '0px'
+        pppp.style.fontSize = '13px'
+        pppp.style.fontWeight = 'bold'
+        pppp.style.color = 'black'
+        pppp1.style.fontSize = '10px'
+        pppp1.style.color = 'black'
+        box64.style.width = '10%'
+        box64.style.backgroundColor = 'transparent'
+        theImageInsideOfTheOrangeBox.style.backgroundposition = 'center'
+        theImageInsideOfTheOrangeBox.style.backgroundsize = 'cover'
+        theImageInsideOfTheOrangeBox.style.backgroundRepeat = 'no repeat'
+        mover2.style.position = 'absolute'
+        mover2.style.width = '30%'
+        mover2.style.height = '30%'
+        mover2.style.marginLeft = '326px'
+        mover2.style.marginTop = '179px'
+        pppp2.style.fontSize = '25px'
+        pppp2.style.color = 'white'
+        pppp3.style.fontSize = '30px'
+        pppp3.style.color = 'white'
+        GrahamLinks.style.backgroundColor = 'white'
+
+    }else if(theImageInsideOfTheOrangeBox.style.backgroundImage = 'url(./img10.png)'){
+        boxMonica.style.width = '47%'
+        mover3.style.width = '55%'
+        mover3.style.height = '100%'
+        mover3.style.marginLeft = '560px'
+        mover3.style.marginTop = '0px'
+    }
+    // boxGraham.style.width = '56%'
+    // theImageInsideOfTheOrangeBox.style.backgroundImage = 'url(./img7.png)'
+    theImageInsideOfTheOrangeBox.style.backgroundposition = 'center'
+    theImageInsideOfTheOrangeBox.style.backgroundsize = 'cover'
+    theImageInsideOfTheOrangeBox.style.backgroundRepeat = 'no repeat'
+    mover2.style.position = 'absolute'
+    mover2.style.width = '30%'
+    mover2.style.height = '30%'
+    mover2.style.marginLeft = '326px'
+    mover2.style.marginTop = '179px'
+    pppp2.style.fontSize = '25px'
+    pppp2.style.color = 'white'
+    pppp3.style.fontSize = '30px'
+    pppp3.style.color = 'white'
+    GrahamLinks.style.backgroundColor = 'white'
+})
+boxMonica.addEventListener('click', () => {
+    
+})
+let box102 = document.querySelector('.box102')
+let body = document.querySelector('body')
+
 
 
 
